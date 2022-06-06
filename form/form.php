4feb22ce583e61a2d7872a5353258e60e6151f5a
Innochka02 <103113428+Innochka02@users.noreply.github.com>
2022-06-06 23:24:00 +0200

    Add files via upload

---
+++ form/form.php
@@ -0,0 +1,31 @@
+
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <title>Authorization and registration</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<!-- Modulo di autorizzazione -->
+
+    <form action="" method="">
+        <label>Username</label>
+        <input type="text" name="login" placeholder="Username">
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Sign in</button>
+        
+        <p>
+            Don't have an account? -->  <a href="register.php"> Create </a>
+        </p>
+
+
+        <div id="exit">
+             <a href="../index.php"> <-- </a>
+        </div>
+
+    </form>
+
+</body>
+</html>
